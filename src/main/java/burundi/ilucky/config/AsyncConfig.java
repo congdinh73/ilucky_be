b5cdf87d012c7fb0603f263fe5756c55066bfab1
unknown <dinhtcpd08605@fpt.edu.vn>
2025-03-05 10:31:53 +0700

    Feat: Add AsyncConfig

---
+++ src/main/java/burundi/ilucky/config/AsyncConfig.java
@@ -0,0 +1,27 @@
+package burundi.ilucky.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+
+    @Bean(name = "customTaskExecutor")
+    public Executor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(5);
+        executor.setMaxPoolSize(10);
+        executor.setQueueCapacity(50);
+        executor.setKeepAliveSeconds(60);
+        executor.setThreadNamePrefix("AsyncExecutor-");
+        executor.setRejectedExecutionHandler(new java.util.concurrent.ThreadPoolExecutor.CallerRunsPolicy()); 
+        executor.initialize();
+        return executor;
+    }
+}
